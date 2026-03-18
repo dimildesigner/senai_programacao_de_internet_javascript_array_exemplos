96b7aa90495fd03cd8be599af1987a533dfce4e1
dimildesigner <dimildesignerdev@gmail.com>
2026-03-18 16:29:35 -0300

    Novos exemplos de Array

--- array.js
+++ array.js
@@ -26,10 +26,54 @@ console.log("\n");
 console.log("-------------\n");
 
 // Métodos de Array
-// Adicionar um elemento a mais ao final do array - Método .push()
+// Adicionar elementos ao final do array - Método .push()
 frutas.push("abacaxi");
 frutas.push("caqui");
 console.log("-- Após a reforma da prateleira --");
 console.log(frutas); 
+console.log("\n");
+console.log("-------------\n");
+
+// Retirar o último elemento - Método .pop()
+frutas.pop("caqui");
+console.log("-- Após a enchente, estragou os caquis --");
+console.log(frutas); 
+console.log("\n");
+console.log("-------------\n");
+
+// Retirar o primeiro elemento - Método .shift()
+frutas.shift();
+console.log("-- Após goteira do teto furado, estragou as uvas --");
+console.log(frutas); 
+console.log("\n");
+console.log("-------------\n");
+
+// Colocar novamente um elemento no início - Método .unshift()
+frutas.unshift("uva");
+console.log("-- Após concerto do teto, trouxe novamente as uvas --");
+console.log(frutas); 
+console.log("\n");
+console.log("-------------\n");
+
+// Para criar novo array contento parte do array original - Método .slice()
+let frutas_meio = frutas.slice(1,3);
+console.log("-- Novas frutas no meio da estante --");
+console.log(frutas_meio); 
+console.log("\n");
+console.log("-------------\n");
+
+// Removo Y elementos a partir da posição - Método .splice(x,y)
+let array = ['a', 'b', 'c', 'd'];
+array.splice(2,1); // Remove 1 elemento a partir do índice 2 ('c')
+console.log("-- Exemplo Splice --");
+console.log(array);  // Resultado: ['a', 'b', 'd']
+console.log("\n");
+console.log("-------------\n");
+
+// Removo X elementos, a partir de sua posição inicial Y - Método .splice(x,y)
+let array2 = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+array2.splice(5,4); // Remove 1 elemento a partir do índice 2 ('c')
+console.log("-- Novo exemplo de .splice --");
+console.log(array2);  // Resultado: ['a', 'b', 'd']
 console.log("\n");
 console.log("-------------\n");
